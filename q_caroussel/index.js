5250abaa8ad8d4d72085090583696531022a980a
Edouaria <e.lebourgeois@onlineformapro.com>
2022-01-27 10:55:37 +0100

    keep mouse down to scroll automatic

--- q_caroussel/index.js
+++ q_caroussel/index.js
@@ -1,8 +1,9 @@
 const list_img = document.getElementsByClassName('logo')
 const list_puce = document.getElementsByClassName('puce')
+let interval_init = -1
 let index = 0
 
-
+// CLICK ONCE TO SEE NEXT IMAGE
 function next(n) {
     list_img[index].classList.remove('active')
     list_puce[index].classList.remove('puce-active')
@@ -15,6 +16,7 @@ function next(n) {
     list_puce[index].classList.add('puce-active')
 }
 
+// CLICK ON BULLET LIST TE SEE IMAGE
 function show(n) {
     list_img[index].classList.remove('active')
     list_puce[index].classList.remove('puce-active')
@@ -22,3 +24,20 @@ function show(n) {
     list_img[index].classList.add('active')
     list_puce[index].classList.add('puce-active')
 }
+
+// KEEP MOUSE DOWN TO SCROLL AUTOMATIC ///////////
+function mouse_down(n) {
+    if (interval_init == -1) {
+        interval_init = setInterval(() => {
+            next(n)
+        }, 100)
+    }
+}
+
+function mouse_up() {
+    if (interval_init != -1) {
+        clearInterval(interval_init)
+        interval_init = -1
+    }
+}
+//////////////////////////////////////////////////
