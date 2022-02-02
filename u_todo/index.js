03a7c84c77f62219d82c1ce4cb0dc4cb01f5a3d5
Edouaria <e.lebourgeois@onlineformapro.com>
2022-02-02 18:55:01 +0100

    add todo_list

--- u_todo/index.js
+++ u_todo/index.js
@@ -1,19 +1,26 @@
 const ul = document.querySelector('ul')
+const form = document.querySelector('form')
+const input = document.querySelector('form > input')
 
-const todos = [
-    {
-        text: 'todo task',
-        done: false
-    },
-    {
-        text: 'todo task 2',
-        done: true
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (input.value != '') {
+        const value = input.value
+        input.value = ''
+        add_task(value)
+        display_todo()
     }
-]
+})
+
+const todos = []
 
 function display_todo() {
     const todos_nodes = todos.map((todo, index) => {
-        return create_todo_node(todo, index)
+        if (todo.edit_mode) {
+            return edit_todo_node(todo, index)
+        } else {
+            return create_todo_node(todo, index)
+        }
     })
     ul.innerHTML = ''
     ul.append(...todos_nodes)
@@ -21,12 +28,83 @@ function display_todo() {
 
 function create_todo_node(todo, index) {
     const li = document.createElement('li')
+
+    const button_delete = document.createElement('button')
+    button_delete.innerHTML = 'Supprimer'
+    button_delete.addEventListener('click', event => {
+        event.stopPropagation()
+        delete_task(index)
+    })
+
+    const button_edit = document.createElement('button')
+    button_edit.innerHTML = 'Editer'
+    button_edit.addEventListener('click', event => {
+        event.stopPropagation()
+        toggle_edit(todo)
+    })
+
     li.innerHTML = `
         <span class="todo ${ todo.done ? 'done' : ''}"></span>
         <p>${ todo.text }</p>
-        <button>Supprimer</button>
     `
+    li.addEventListener('click', () => {
+        toggle_done_task(index)
+    })
+    li.appendChild(button_edit)
+    li.appendChild(button_delete)
+
     return li
+}
+
+function edit_todo_node(todo, index) {
+    const li = document.createElement('li')
+    const input = document.createElement('input')
+    input.type = 'text'
+    input.value = todo.text
+
+    const button_save = document.createElement('button')
+    button_save.innerHTML = 'Save'
+    button_save.addEventListener('click', () => {
+        edit_task(todo, input)
+    })
+
+    const button_cancel = document.createElement('button')
+    button_cancel.innerHTML = 'Cancel'
+    button_cancel.addEventListener('click', () => {
+        toggle_edit(todo)
+    })
+
+    li.append(input, button_save, button_cancel)
+    return li
+}
+
+function add_task(text) {
+    todos.push({
+        text,
+        done: false,
+    })
+}
+
+function delete_task(index) {
+    todos.splice(index, 1)
+    display_todo()
+}
+
+function toggle_done_task(index) {
+    todos[index].done = !todos[index].done
+    display_todo()
+}
+
+function toggle_edit(todo) {
+    todo.edit_mode = !todo.edit_mode
+    display_todo()
+}
+
+function edit_task(todo, input) {
+    const value = input.value
+    todo.text = value
+    todo.edit_mode = false
+    display_todo()
 }
 
 display_todo()
