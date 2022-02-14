75d777425e22921703f21c45c2f8fac6db0d425c
Edouaria <e.lebourgeois@onlineformapro.com>
2022-02-14 12:16:46 +0100

    refactoring with for of

--- ab_letter_pair/index.js
+++ ab_letter_pair/index.js
@@ -1,11 +1,16 @@
 const alphabet = 'abcdefghifklmnopqrstuvwxyz'
+let count = 0
 
 function letter_pair() {
-    for (let i = 0; i < alphabet.length; i++) {
-        for (let j = 0; j < alphabet.length; j++) {
-            console.log(alphabet[i]+alphabet[j])
-        }
+    for (const letter_1 of alphabet) {
+        for (const letter_2 of alphabet) {
+            if (letter_1 != letter_2) {
+                console.log(letter_1 + letter_2);
+                count += 1
+            }
+        }    
     }
+    console.log(count);
 }
 
 letter_pair()
