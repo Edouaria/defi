c890ce5a823d7a18f537443a2d15300dd2afe705
Edouaria <e.lebourgeois@onlineformapro.com>
2022-07-26 11:23:33 +0100

    add regex to prevent from special caracter

--- 25_palindrome/index.js
+++ 25_palindrome/index.js
@@ -1,9 +1,13 @@
+// let re = /[^A-Za-z0-9]/g
+let re = /[\W_]/g
+
 function palindrome(word) {
     if (word == undefined) {
         console.log('veuillez rentrer un mot');
     } else {
-        const word_reverse = [...word].reverse().join('')
-        if (word_reverse == word) {
+        const clear_word = word.replace(re, '')
+        const word_reverse = [...clear_word].reverse().join('')
+        if (word_reverse == clear_word) {
             console.log(`${word} est un palindrome`);
         } else {
             console.log(`${word} n'est pas un palindrome`);
